cdf2ca7a8819738ab2ab604377693bcc6e7b1690
Jenausmax Jen <mminmax@gmail.com>
2020-12-02 16:43:24 +0300

    Добавьте файлы проекта.

---
+++ MySite/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySite.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string NamePost { get; set; }
+        public string DescriptionPost { get; set; }
+        public string Time { get; set; }
+        public string[] Tags { get; set; }
+        public object Image { get; set; }
+
+    }
+}
